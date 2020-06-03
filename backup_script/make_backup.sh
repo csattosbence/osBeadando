83f6b83242af61d3168158d0f7a517b2d79d0c21
Bence <csattos.bence@gmail.com>
2020-06-03 21:45:06 +0200

    variable name fix

---
+++ backup_script/make_backup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+## hetente mentéseket készít a kiválasztott directory-ról.
+
+##válltozók
+##log file
+log_loc="/var/log/mybackup.log"
+##
+function check_dir_loc {
+  if [ ! -s  "/backup_dirs.conf" ]
+  then
+    echo "Kérem készítsen egy listát azokról a mappákról/directory-ról amről mentést szeretne készíteni.
+    Készítsen egy backup_dirs.conf nevű fájlt a root directory-ban"
+    exit ]
+  fi
+}
+function check_backup_loc {
+  if [ ! -s "/backup_loc.conf" ]
+  then
+    echo "Kérem adja meg, hogy hol szeretné tárolni a mentési fájlokat.
+    Készitsen egy backup_loc.conf nevű fájlt a root directory-ban"
+    exit ]
+  fi
+}
+function check_schedule {
+  #nézzük meg hogy a script megtalálható-e a hetente lefutó scriptek között
+  if [ ! -s "/etc/cron.weekly/make_backup" ]
+  then
+    #ha nincs benne a directoryban akkor másoljuk át a scriptet
+    sudo cp make_backup.sh /etc/cron.weekly/make_backup
+    echo "A fájl mentés be lett állitva heti mentésre"
+    exit ]
+  fi
+
+}
+function perform_backup {
+  #eltároljuk a mentési helyet
+  backup_path=$(cat /backup_loc.conf)
+
+  echo "A mentés elkezdődött...">$log_loc
+  #minen egyes directory-t letömör0tünk a mentési heylre
+  while read dir_path
+  do
+  dir_name=$(basename $dir_path)
+
+  filename=$backup_path$dir_name.tar.gz
+  tar -zcf $filename $dir_path 2>>$log_loc
+
+  echo "A $dir_name mentése elkészült." >> $log_loc
+
+  done < /backup_dirs.conf
+
+  echo "A mentés befejeződött ekkor:" >> $log_loc
+  date >> $log_loc
+}
+
+check_dir_loc
+check_backup_loc
+check_schedule
+perform_backup
